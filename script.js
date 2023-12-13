c86e65099da878df49d8875bd7fc92f0e8789057
Daniel Servino da Rocha <142444338+DanielSRocha@users.noreply.github.com>
2023-12-13 11:28:34 +0000

    criação do script do site usando javascript

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+function celcius() {
+    let celcius = document.getElementById('entrada').value;
+    celcius = (celcius * 1.8) + 32;
+    document.getElementById('resultado').textContent = celcius
+}
